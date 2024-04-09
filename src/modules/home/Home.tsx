8afcbf55e5a04dfb5fd345cb53d9c4a53070fb58
freetimezp <kraftukrnet@ukr.net>
2024-04-09 18:37:12 +0300

    04. js-portfolio-69-saas-newsletter. Branding component.

--- src/modules/home/Home.tsx
+++ src/modules/home/Home.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Header from '@/shared/widgets/header/Header';
 import Banner from './elements/Banner';
+import Branding from './elements/Branding';
 
 const Home = () => {
     return (
         <div>
             <Header />
             <Banner />
+            <Branding />
         </div>
     );
 }
